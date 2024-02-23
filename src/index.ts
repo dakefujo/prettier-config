4257a181aad56355776fc7b9a91bcbaa6f0ae7d2
glamseam <glamseam@gmail.com>
2024-02-24 04:29:29 +0900

    release: v0.1.0

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { type Config } from 'prettier'
+
+const config: Config = {
+    plugins: [
+        'prettier-plugin-organize-imports'
+    ],
+    printWidth: 80,
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    singleAttributePerLine: true
+}
+
+export default config
