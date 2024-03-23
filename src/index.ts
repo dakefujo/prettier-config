af8ec2cf6bd7aca1efcd5b7dc01879d7ab157bb6
glamseam <glamseam@gmail.com>
2024-03-23 13:40:51 +0900

    fix: Config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { type Config } from 'prettier'
 
 const config: Config = {
-    plugins: [
-        'prettier-plugin-organize-imports'
-    ],
+    plugins: ['prettier-plugin-organize-imports'],
     printWidth: 80,
     trailingComma: 'none',
     tabWidth: 4,
     semi: false,
     singleQuote: true,
-    singleAttributePerLine: true
+    singleAttributePerLine: true,
+    overrides: [
+        {
+            files: ['*.css', '*.scss'],
+            options: {
+                printWidth: 120
+            }
+        }
+    ]
 }
 
 export = config
